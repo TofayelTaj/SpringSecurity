1688c6cfa4067eabb2126da9b14cd4e4d51f8dc9
Tofayel Ahmed <tajuddinahmed450@gmail.com>
2022-03-30 18:47:44 +0600

    basic auth with NoOpPasswordEncoder

--- src/main/java/com/example/Spring/Security/Security/ApplicationSecurityConfiger.java
+++ src/main/java/com/example/Spring/Security/Security/ApplicationSecurityConfiger.java
@@ -1,17 +1,28 @@
 package com.example.Spring.Security.Security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfiger extends WebSecurityConfigurerAdapter {
 
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfiger(PasswordEncoder passwordEncoder) {
+       this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
 
         http
                 .authorizeRequests()
@@ -21,6 +32,18 @@ public class ApplicationSecurityConfiger extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth
+                .inMemoryAuthentication()
+                .withUser("taj")
+                .password("password")
+                .roles("STUDENT");
+
 
     }
 }
